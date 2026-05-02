d20b9dc5f944dab0e976fc0fcc219a9cc6cd0813
Anindya Sarkar <sanindya710@gmail.com>
2026-05-02 14:25:30 +0530

    Changed the structure of files

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import About_page from './Pages/About.jsx'
 import { Routes, Route } from 'react-router-dom'
 import Nav from './Navbar.jsx';
-import Brands from './About_components/about_brands';
+import About_page from './Pages/About.jsx'
 import ContactPage from './Contact_components/Contact-page';
 
 
 function App() {
   return (<>
     <Nav />
-
     <Routes>
       <Route path='/about' element={<About_page />}></Route>
       <Route path='/connect' element={<ContactPage />}></Route>
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,12 +1,9 @@
-import Intro from '../About_components/about_intro'
-import Brands from '../About_components/about_brands'
 import Nav from '../Navbar.jsx'
+import Intro from '../About_components/about_intro'
 import AboutTeam from '../About_components/about-team'
 
 export default function About_page(){
     return(<>
     <Intro/>
-    <Brands/>
     <AboutTeam/>
-    </>)
-}
+    </>)}
