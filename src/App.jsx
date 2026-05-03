c99c83edabeb50158ff061fdd4986b9cd3a1d118
Anindya Sarkar <sanindya710@gmail.com>
2026-05-03 11:32:59 +0530

    New attractive Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Nav from './Navbar.jsx';
 import About_page from './Pages/About.jsx'
 import ContactPage from './Contact_components/Contact-page';
 
+import Home from './Pages/Home.jsx';
 
 function App() {
   return (<>
     <Nav />
     <Routes>
+      <Route path='/' element={<Home/>}></Route>
       <Route path='/about' element={<About_page />}></Route>
       <Route path='/connect' element={<ContactPage />}></Route>
     </Routes>
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,8 @@
+import '../CSS/Home.css';
+import Home_intro from '../Home_components/Home_intro.jsx';
+
+export default function Home(){
+    return(<div>
+        <Home_intro />
+    </div>)
+}
